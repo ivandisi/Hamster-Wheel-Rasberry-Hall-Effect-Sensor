30738cfdb10d58ab6336ddc828d89d358aba1f8b
ivandisi <ivan.disir@gmail.com>
2025-12-24 09:53:25 +0100

    move to interrupt mode, refactoring

--- pistacchio.py
+++ pistacchio.py
@@ -1,16 +1,51 @@
-import RPi.GPIO as GPIO
 import time
 import threading
 import json
-from time import sleep
 from tinydb import TinyDB, Query
 from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+from gpiozero import Button
+from signal import pause
 
-db = TinyDB('pistacchiodb.json')
+db = TinyDB('pistacchiodbnew.json')
 stop_event = threading.Event()
-tripLength = 29
+tripLength = 91
+Digital_PIN = 22
+
+sensor = Button(Digital_PIN, pull_up=True, bounce_time=0.0009)
+sensor.when_pressed = lambda: myCounter()  
+
+def getTripsByYear(year):
+    arr = []
+    for number in range(1, 13):
+        n = f"{number:02d}"
+        data = getTripsByMonth(year + n, n)
+        arr.append(data)
+    return arr
+
+def getTripsByMonth(req, month):
+    Qry = Query()
+    t = db.search(Qry.data.test(lambda d: d.startswith(req)))
+    return {'trips': len(t), 'length': (len(t) * tripLength), 'month': month}
+
+def getTripsByDay(day):
+    arr = []
+    for number in range(24):
+        n = f"{number:02d}"
+        data = getTripsByHour(day, n)
+        arr.append(data)
+    return arr
+
+def getTripsByHour(day, hour):
+    Qry = Query()
+    data = db.search((Qry.data == day) & Qry.hour.test(lambda h: h.startswith(hour)))
+    return {'trips': len(data), 'length': (len(data) * tripLength), 'hour': hour}
+
+def myCounter():
+    print('Pistacchio ' + str(time.time()))
+    data = datetime.fromtimestamp(time.time())
+    db.insert({'type': 'trip', 'time': time.time(), 'data': data.strftime("%Y%m%d"), 'hour': data.strftime("%H:%M")})
 
 class ApiHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -25,14 +60,7 @@ class ApiHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(b"Parametro 'day' mancante")
                 return
-
             result = getTripsByDay(day)
-            
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(result).encode())
-
         elif path == "/getByYear":
             year = params.get("year", [None])[0]
             if not year:
@@ -40,120 +68,58 @@ class ApiHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(b"Parametro 'year' mancante")
                 return
-
             result = getTripsByYear(year)
-
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(result).encode())
-            
         else:
             self.send_response(404)
             self.end_headers()
             self.wfile.write(b"Endpoint non trovato")
+            return
 
-def getTripsByYear(year):
-  arr = []
-  for number in range(1, 13):
-    n = f"{number:02d}"
-    data = getTripsByMonth(year + n)
-    arr.append(data)
-  return arr
-  
-def getTripsByMonth(month):
-  Qry = Query()
-  t = db.search(Qry.data.test(lambda d: d.startswith(month)))
-  return {'trips': len(t), 'length': (len(t) * tripLength)}
-
-def getTripsByMinute(day, hour):
-  Qry = Query()
-  data = db.search((Qry.data == day) &  Qry.hour.test(lambda h: h.startswith(hour)))
-  return data
-
-def getTripsByDay(day):
-  arr = []
-  for number in range(24):
-    n = f"{number:02d}"
-    data = getTripsByHour(day, n)
-    arr.append(data)
-  return arr
-  
-def getTripsByHour(day, hour):
-  Qry = Query()
-  data = db.search((Qry.data == day) &  Qry.hour.test(lambda h: h.startswith(hour)))
-  return {'trips': len(data), 'length': (len(data) * tripLength)}
-
-def myCounter():
-  print('Pistacchio ' + str(time.time()))
-  data = datetime.fromtimestamp(time.time())
-  db.insert({'type': 'trip', 'time': time.time(), 'data': data.strftime("%Y%m%d"), 'hour': data.strftime("%H:%M")})
-
-def gpioThread():
-  try:
-    while not stop_event.is_set():
-      if GPIO.input(Digital_PIN):
-        myCounter()
-      sleep(0.025)
-  finally:
-    GPIO.cleanup()
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(result).encode())
 
 def serverThread():
-  server = HTTPServer(("0.0.0.0", 8000), ApiHandler)
-  print("Server avviato su http://localhost:8000")
-  server.serve_forever()
-  
+    server = HTTPServer(("0.0.0.0", 8000), ApiHandler)
+    print("Server avviato su http://localhost:8000")
+    server.serve_forever()
+
 def commandThread():
-  try:
-    while not stop_event.is_set():
-      cmd = input("Command: ")
-      cmd = cmd.strip()
-      
-      if cmd == "q":
+    try:
+        while not stop_event.is_set():
+            cmd = input("Command: ").strip()
+            if cmd == "q":
+                stop_event.set()
+            elif cmd == "r":
+                cmdTime = input("hour: ")
+                Qry = Query()
+                print(db.search(Qry.hour == cmdTime))
+            elif cmd == "trips":
+                cmdTime = input("date for Trips (yyyymmdd): ")
+                print(getTripsByDay(cmdTime))
+            elif cmd == "mtrips":
+                cmdTime = input("year for Trips (yyyy): ")
+                print(getTripsByYear(cmdTime))
+            elif cmd == "t":
+                myCounter()
+            elif cmd == "a":
+                print(db.all())
+            time.sleep(0.5)
+    except KeyboardInterrupt:
         stop_event.set()
-      if cmd == "r":
-        cmdTime = input("hour: ")
-        Qry = Query()
-        print(db.search(Qry.hour == cmdTime))
-      if cmd == "trips":
-        cmdTime = input("date for Trips (yyyymmdd): ")
-        print(getTripsByDay(cmdTime))
-      if cmd == "mtrips":
-        cmdTime = input("year for Trips (yyyy): ")
-        print(getTripsByYear(cmdTime))
-      if cmd == "t":
-        myCounter(trips)
-      if cmd == "a":
-        print(db.all())
-        
-      sleep(0.5)
-      
-  except KeyboardInterrupt:
-    print('\nScript end! total trips ' + str(trips))
-  finally:
-    stop_event.set()   
-    GPIO.cleanup()
-    
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-Digital_PIN = 22
-trips = 0;
-
-GPIO.setup(Digital_PIN, GPIO.IN)
 
 print('[CTRL + C, to stop the Script!]')
 
 t0 = threading.Thread(target=serverThread, daemon=True)
-t1 = threading.Thread(target=gpioThread)
 t2 = threading.Thread(target=commandThread)
 
 t0.start()
-t1.start()
 t2.start()
 
-t0.join()
-t1.join()
-t2.join()
-
-
+try:
+    pause() 
+except KeyboardInterrupt:
+    stop_event.set()
+finally:
+    print("Script terminato")
